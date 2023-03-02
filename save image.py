aa7773a6aad9a014ccf921785c48d35f72827cf0
Nikita <Pikkas.Nikita@gmail.com>
2023-03-02 21:33:16 +0200

    töötab

--- save image.py
+++ save image.py
@@ -3,35 +3,43 @@ import numpy as np
 from PIL import Image
 from math import atan
 import math
-
+from utils import ImageProcessor
 
 """
 To train model go to this link https://amin-ahmadi.com/cascade-trainer-gui/
 """
-from utils import ImageProcessor
+
 
 CAMERA = True
-vid = cv.VideoCapture(0, cv.CAP_DSHOW)
+vid = cv.VideoCapture(1, cv.CAP_DSHOW)
 # Set camera parameters
 vid.set(cv.CAP_PROP_FPS, 60)
-vid.set(cv.CAP_PROP_CONTRAST, 250)  # Set contrast to 0.8
-vid.set(cv.CAP_PROP_BRIGHTNESS, 100)  # Set brightness to 0.5
-vid.set(cv.CAP_PROP_SATURATION, 250)  # Set saturation to 0.6
-vid.set(cv.CAP_PROP_HUE, 250)  # Set hue to 0.3
+vid.set(cv.CAP_PROP_CONTRAST, 255)  # Set contrast to 0.8
+vid.set(cv.CAP_PROP_BRIGHTNESS, 0)  # Set brightness to 0.5
+vid.set(cv.CAP_PROP_SATURATION, 1000)  # Set saturation to 0.6
+vid.set(cv.CAP_PROP_HUE, 1000)  # Set hue to 0.3
 vid.set(cv.CAP_PROP_FRAME_WIDTH, 640)  # Set width to 640
 vid.set(cv.CAP_PROP_FRAME_HEIGHT, 480)  # Set height to 480
 
+
+
+# processor = ImageProcessor("My Image")
+
 while True:
-    ret, im = vid.read()
-    im = cv.rotate(im, cv.ROTATE_180)
+    ret, frame = vid.read()
+    frame = cv.rotate(frame, cv.ROTATE_180)
 
-    hsv = cv.cvtColor(im, cv.COLOR_BGR2HSV)
-    gray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
+    hsv = cv.cvtColor(frame, cv.COLOR_RGB2HSV)
+    blurred = cv.GaussianBlur(hsv, (5, 5), 0)
+    gray = cv.cvtColor(blurred, cv.COLOR_BGR2GRAY)
+    thresh = cv.threshold(gray, 100, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
+    # processor.process_image(frame)
+    # processor.show_image()
+
+    cv.imshow("image", thresh)
+    # cv.imshow("tresh", thresh)
 
 
-
-    cv.imshow("thresh1", im)
-    cv.imshow("mage", gray)
 
     if cv.waitKey(1) == ord('q'):
         cv.destroyAllWindows()
